ac6b77a127c03a82d3d7a0ee09916b8802bd3240
hongchiong <hongchiong@gmail.com>
2017-04-03 20:02:34 +0800

    initialized html and javascript. And created sketchpad divs

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-First Git Project
+Sketch!
---
+++ scripts.js
@@ -0,0 +1,19 @@
+function drawGrid(squaresPerSide) {
+
+    //Add rows
+    for (var i = 0; i < squaresPerSide; i++) {
+        $('#sketchpad').append('<div class="row"></div>');
+    }
+
+    //Fill rows
+    for (var i = 0; i < squaresPerSide; i++) {
+        $('.row').append('<div class="square"></div>');
+    }
+
+    //Set square sizes
+    var squareDimension = $('#sketchpad').width() / squaresPerSide;
+    $('.square').css({
+        'height': squareDimension,
+        'width': squareDimension
+    });
+}
